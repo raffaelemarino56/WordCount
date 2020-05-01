f81277d0b6191654740eab467e93db963b7eea28
Raffaele Marino <33875039+raffaelemarino56@users.noreply.github.com>
2020-05-01 20:16:28 +0200

    1.1
    added a function that search in folder each file, and put each word, of that file, in a array dinamically allocated

--- WordCount.c
+++ WordCount.c
@@ -2,9 +2,101 @@
 #include <string.h>
 #include "mpi.h"
 #include "stdlib.h"
+#include "dirent.h"
+#include <unistd.h>
 
-#define row 3
+#define row 1
 #define cols 1
+
+void deallocaArrayMultiDim(int**x, int rows){
+  for (int i=0; i<rows; i++)
+  {
+    free(x[i]);
+  }
+  free(x);
+}
+
+char** creaArrayParole(){
+    int i=0;
+    int j=0;
+    char ch;
+    char separatore='\n';
+    DIR *dir;
+    struct dirent *ent;
+    char cwd[PATH_MAX];
+
+    //inizializzo l'array
+    char **parole;
+    parole=(char**)malloc(row*sizeof(char*));
+    for(int alfa=0;alfa<row;alfa++)
+        parole[alfa]=malloc(cols*sizeof(char));
+   
+    //get path of current directory
+    if (getcwd(cwd, sizeof(cwd)) != NULL) {
+       printf("Current working dir: %s\n", cwd);
+    } else {
+       perror("getcwd() error");
+       
+    }
+    //add path folder 
+    strcat(cwd,"/file/");
+
+    if ((dir = opendir(cwd)) != NULL) {
+    /* print all the files and directories within directory */
+    int numfile=0;
+        //untill the dir is empty, so for each file i take the word and put it all in the array
+        //after this array will be splitted equally for each processor
+        while ((ent = readdir (dir)) != NULL) {
+            if(strcmp(ent->d_name,".")!=0 && strcmp(ent->d_name,"..")!=0){
+                FILE *in_file;
+                char stringa[30]="file/";
+                strcat(stringa,ent->d_name);
+                printf("%s\n",stringa);
+                in_file = fopen(stringa, "r");
+                    // test for files not existing. 
+                    if (in_file == NULL) 
+                    {   
+                        printf("Error! Could not open file\n"); 
+                        exit(-1); // must include stdlib.h 
+                    }
+
+                    while((ch = fgetc(in_file)) != EOF)
+                    {   
+                        if(ch==separatore)
+                        {   
+                            parole[i][j]='\n';
+                            i++;
+                            //se supero il numero di righe, quindi ho più parole di quante ne ho assegnate inizialmente
+                            //non essendo ancora finito il file, gli aggiugno un'altra riga per l'iterazione successiva
+                            //farlo tante volte mi fa perdere un po' di tempo ma non spreco memoria    
+                            if(i>=row){
+                                parole=realloc(parole,sizeof(char*)*(i+1));
+                                parole[i]=malloc(cols*sizeof(char)); 
+                            }
+                            j=0;
+                        }else{
+                            parole[i][j]=ch;
+                            j++;
+                            //se supero il numero di colonne, quindi la lunghezza della parola, non essendo ancora 
+                            //la parola finita, allora aggiugno 1 altro spazio per l'iterazione successiva
+                            //farlo tante volte mi fa perdere un po' di tempo ma non spreco memoria
+                            if(j>=cols){
+                                parole[i]=realloc(parole[i],sizeof(char)*(j+1));   
+                            }   
+                        }   
+                    }
+                fclose(in_file);
+                }                
+            }
+        closedir (dir);
+        } else {
+            /* could not open directory */
+            printf("Error! Could not open directory\n"); 
+            exit(-1); // must include stdlib.h 
+        }
+
+    return parole;
+}
 
 int main(int argc , char* argv[]){
     /*
@@ -16,74 +108,21 @@ int main(int argc , char* argv[]){
     MPI_Init (&argc , &argv);
     MPI_Comm_rank(MPI_COMM_WORLD , &my_rank);
     */
-
-    //crea nome file per poterli aprire
-    for(int p=0;p<10;p++){
-        char nomeFile[]="file";
-        char numero=p+'0';
-        strncat(nomeFile,&numero,1);
-        printf("%s\n",nomeFile);
-    }
-
-    FILE *in_file  = fopen("file/provaIn.txt", "r"); // read only 
-    //FILE *out_file = fopen("~/provaOut.txt", "w"); // write only 
-    int i=0;
-    int j=0;
     int k=0;
-    char ch;
-    char separatore='\n';
-
+    int numParole=0;
+   
     //matrice di parole
     char **parole;
-    parole=(char**)malloc(row*sizeof(char*));
-    for(int alfa=0;alfa<row;alfa++)
-        parole[alfa]=malloc(cols*sizeof(char));
-
-    // test for files not existing. 
-    if (in_file == NULL) 
-    {   
-        printf("Error! Could not open file\n"); 
-        exit(-1); // must include stdlib.h 
-    }
 
     //inserisco nell'array di stringhe ogni parola
-    while((ch = fgetc(in_file)) != EOF)
-    {   
-        if(ch==separatore)
-        {   
-            parole[j][i]='\n';
-            j++;    
-            //se supero il numero di righe, quindi ho più parole di quante ne ho assegnate inizialmente
-            //non essendo ancora finito il file, gli aggiugno un'altra riga per l'iterazione successiva
-            //farlo tante volte mi fa perdere un po' di tempo ma non spreco memoria
-            if(j>=row){
-                parole=realloc(parole,sizeof(char*)*(j+1));
-                parole[j]=malloc(cols*sizeof(char)); 
-            }
-            i=0;
-        }else{
-            parole[j][i]=ch;
-            i++;
-            //se supero il numero di colonne, quindi la lunghezza della parola, non essendo ancora 
-            //la parola finita, allora aggiugno 1 altro spazio per l'iterazione successiva
-            //farlo tante volte mi fa perdere un po' di tempo ma non spreco memoria
-            if(i>=cols){
-                parole[j]=realloc(parole[j],sizeof(char)*(i+1));   
-            }   
-        }   
-    }    
+    parole=creaArrayParole();
 
-    //stampo parole, solo per controllo, si può poi eliminare 
-    for(int h=0;h<j;h++){
-        k=0;
-        while(parole[h][k]){
-            printf("%c",parole[h][k]);
-            k++;
+    //stampo parole, solo per controllo, si può poi eliminare
+    int num=0;
+    while(parole[num]){
+        printf("%s",parole[num]);
+        num++;
         }
-    }
-
-    fclose(in_file);
-    //fclose(out_file);    
     
     //MPI_Finalize ();
     return 0;
