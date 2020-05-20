36e3039cada56fb46bb22344f2d65006d468a957
Raffaele Marino <33875039+raffaelemarino56@users.noreply.github.com>
2020-05-20 19:50:49 +0200

    1.0

--- multival.c
+++ multival.c
@@ -41,27 +41,12 @@ int main (int argc, char *argv[])
     MPI_Type_commit(&wordtype); 
 
     if (numtasks == 4) {
-    if (rank == 0) {
+        if (rank == 0) {
 
-        strcpy(a[0].parola,"aaa");
-        strcpy(a[1].parola,"bbb");
-        strcpy(a[2].parola,"ccc");
-        strcpy(a[3].parola,"ddd");
-        strcpy(a[4].parola,"eee");
-        strcpy(a[5].parola,"fff");
-        strcpy(a[6].parola,"ggg");
-        strcpy(a[7].parola,"hhh");
-        strcpy(a[8].parola,"iii");
-        strcpy(a[9].parola,"jjj");
-        strcpy(a[10].parola,"kkk");
-        strcpy(a[11].parola,"lll");
-        strcpy(a[12].parola,"mmm");
-        strcpy(a[13].parola,"nnn");
-        strcpy(a[14].parola,"ooo");
-        strcpy(a[15].parola,"ppp");
-        for(int j=0;j<row;j++){
-            a[j].frequenza=j;
-        }
+            for(int j=0;j<row;j++){
+                sprintf(a[j].parola, "%d", j);
+                a[j].frequenza=j;
+            }
 
             for (i=0; i<numtasks; i++){
                 start=i*numtasks;
@@ -72,7 +57,7 @@ int main (int argc, char *argv[])
         MPI_Recv(a, 4, wordtype, source, tag, MPI_COMM_WORLD, &stat);
         printf("rank= %d , PAROLE = b[0] = %s , b[1] = %s , b[2] = %s , b[3] = %s\n",rank,a[0].parola,a[1].parola,a[2].parola,a[3].parola);
         printf("rank= %d , FREQUENZE = b[0] = %d , b[1] = %d , b[2] = %d , b[3] = %d\n",rank,a[0].frequenza,a[1].frequenza,a[2].frequenza,a[3].frequenza);
-        printf("\n");
+        
     }else{
         printf("Must specify %d processors. Terminating.\n",4);
     }
