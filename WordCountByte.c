39a356e99c250958442370af544601f78564e8e1
Raffaele Marino <33875039+raffaelemarino56@users.noreply.github.com>
2020-05-29 19:28:43 +0200

    1.1
    Now I'm able to send the struct from one (master) to the all other process, after that i need to put the word in Word struct, then calculate the occurence (in each process) and send to the master, that calculate the final occurance and return a csv.

--- WordCountByte.c
+++ WordCountByte.c
@@ -5,130 +5,40 @@
 #include <stdlib.h>
 #include <dirent.h>
 #include <sys/stat.h>
+#include <time.h>
+#include <mpi.h>
 
 
-void fseek_filesize(const char *filename){
-    FILE *fp = NULL;
-    long off;
+#define row 200000 //change this if the word for all file exceed (or is lowere) this value
+                  //for now this is a static program, just beacuse i found some difficult with c 
+                  //and the dinamic allocation of the memory
+#define cols 16   //max lenght of the word
 
-    fp = fopen(filename, "r");
-    if (fp == NULL)
-    {
-        printf("failed to fopen %s\n", filename);
-        exit(EXIT_FAILURE);
-    }
-
-    if (fseek(fp, 0, SEEK_END) == -1)
-    {
-        printf("failed to fseek %s\n", filename);
-        exit(EXIT_FAILURE);
-    }
-
-    off = ftell(fp);
-    if (off == (long)-1)
-    {
-        printf("failed to ftell %s\n", filename);
-        exit(EXIT_FAILURE);
-    }
-
-    printf("[*] fseek_filesize - file: %s, size: %ld\n", filename, off);
-
-    if (fclose(fp) != 0)
-    {
-        printf("failed to fclose %s\n", filename);
-        exit(EXIT_FAILURE);
-    }
-}
-
-void fstat_filesize(const char *filename){
-    int fd;
-    struct stat statbuf;
-
-    fd = open(filename, O_RDONLY, S_IRUSR | S_IRGRP);
-    if (fd == -1)
-    {
-        printf("failed to open %s\n", filename);
-        exit(EXIT_FAILURE);
-    }
-
-    if (fstat(fd, &statbuf) == -1)
-    {
-        printf("failed to fstat %s\n", filename);
-        exit(EXIT_FAILURE);
-    }
-
-    printf("[*] fstat_filesize - file: %s, size: %ld\n", filename, statbuf.st_size);
-
-    if (close(fd) == -1)
-    {
-        printf("failed to fclose %s\n", filename);
-        exit(EXIT_FAILURE);
-    }
-}
-
-void seek_filesize(const char *filename){
-    int fd;
-    off_t off;
-
-    if (filename == NULL)
-    {
-        printf("invalid filename\n");
-        exit(EXIT_FAILURE);
-    }
-
-    fd = open(filename, O_RDONLY, S_IRUSR | S_IRGRP);
-    if (fd == -1)
-    {
-        printf("failed to open %s\n", filename);
-        exit(EXIT_FAILURE);
-    }
-
-    off = lseek(fd, 0, SEEK_END);
-    if (off == (off_t)-1)
-    {
-        printf("failed to lseek %s\n", filename);
-        exit(EXIT_FAILURE);
-    }
-
-    printf("[*] seek_filesize - file: %s, size: %ld\n", filename, off);
-
-    if (close(fd) == -1)
-    {
-        printf("failed to close %s\n", filename);
-        exit(EXIT_FAILURE);
-    }
-}
-
+#define splitprocesso 20
+#define numfile 10
 
 typedef struct {
 	int rank;
-    signed long start;
-    signed long end;
-    char nomefile[10][16];
-}SplitPerProcesso;
-
-typedef struct {
-	int rank;
-    signed long start;
-    signed long end;
+    long start;
+    long end;
     char nomefile[16];
 }SplitPerProcesso2;
 
 typedef struct{
-    signed long size;
+    long size;
     char nomefile[16];
 }SizeFile;
 
-SplitPerProcesso2 BPP2(long * bytePerProcesso , SizeFile * bytePerFile, int p){
+void BPP2(SplitPerProcesso2 * s , long * bytePerProcesso , SizeFile * bytePerFile, int p){
     int i=0; //processi
     int j=0; //struttura
     int k=0; //file
     int n=0; //num file in struttura
     long rimanenza=0;
     long tot = 0;
-    SplitPerProcesso2 s[20];
-
+    
     while(i<p){
+        s[j].rank=i;
         s[j].start=rimanenza;
         signed long differenza = bytePerProcesso[i]-bytePerFile[k].size;      
         if(differenza>0 && k<10){   //se la taglia del processo è abbastanza grande 
@@ -141,9 +51,6 @@ SplitPerProcesso2 BPP2(long * bytePerProcesso , SizeFile * bytePerFile, int p){
             rimanenza=0;
             j++;
             k++;
-
-            
-
         }else{
             signed long diff2=bytePerFile[k].size-bytePerProcesso[i];
             if(diff2<0){//quando k = 10, ovvero sono all'ultimo file
@@ -163,6 +70,7 @@ SplitPerProcesso2 BPP2(long * bytePerProcesso , SizeFile * bytePerFile, int p){
             }
             strcpy(s[j].nomefile,bytePerFile[k].nomefile);
             printf(", con file %s , parto da %ld, arrivo a %ld , rimangono al processo %ld\n",s[j].nomefile,s[j].start,s[j].end,bytePerProcesso[i]);
+            printf("\n");
 
             n=0;
             i++;
@@ -170,53 +78,10 @@ SplitPerProcesso2 BPP2(long * bytePerProcesso , SizeFile * bytePerFile, int p){
             s[j].start=rimanenza;
         }
     }
-    return *s;
+    
 }
 
-SplitPerProcesso BPP(long * bytePerProcesso , SizeFile * bytePerFile, int p){
-    int i=0; //struttura e processi
-    int k=0; //file
-    int n=0; //num file in struttura
-    long tot = 0;
-    SplitPerProcesso s[p];
-
-    while(i<p){
-
-        if(i==0){
-            s[i].start=0;
-        }
-
-        signed long differenza = bytePerProcesso[i]-bytePerFile[k].size;      
-        if(differenza>0 && k<10){
-
-            bytePerProcesso[i]=bytePerProcesso[i]-bytePerFile[k].size;
-            tot += bytePerFile[k].size;
-            strcpy(s[i].nomefile[n],bytePerFile[k].nomefile);
-            n++;
-            k++;
-
-        }else{
-            signed long diff2=bytePerFile[k].size-bytePerProcesso[i];
-            if(diff2<0){
-                tot += bytePerFile[k].size;
-                s[i].end=tot;
-            }else{
-                bytePerFile[k].size=bytePerFile[k].size-bytePerProcesso[i];
-                tot+=bytePerProcesso[i];
-                s[i].end=tot-1;
-            }
-            strcpy(s[i].nomefile[n],bytePerFile[k].nomefile);
-            
-            n=0;
-            i++;
-            s[i].start=tot;
-        }
-    }
-    return *s;
-}
-
-void ripartizioneElementi( signed long * arrayAppoggio, signed long sizeFile, int p){
-
+void ripartizioneElementi( long * arrayAppoggio,  long sizeFile, int p){
     int modulo=sizeFile%p;  
     //è probiabile che per solo per alcuni processi avrò più elementi   
     
@@ -245,7 +110,7 @@ void ripartizioneElementi( signed long * arrayAppoggio, signed long sizeFile, in
             }
             modulo--;
         }
-        //free(temp);
+       
     }else{
         //altrimenti riempio normalmente questo array di appoggio con i valori della divisione,
         //esendo uguali per tutti i processi
@@ -253,7 +118,6 @@ void ripartizioneElementi( signed long * arrayAppoggio, signed long sizeFile, in
             arrayAppoggio[i]=sizeFile/p;
         }
     }
-    //free(modulo);
 }
 
 
@@ -266,20 +130,14 @@ double stat_filesize(const char *filename){
         exit(EXIT_FAILURE);
     }
 
-    //printf("[*] stat_filesize - file: %s, size: %ld\n", filename, statbuf.st_size);
-
     return statbuf.st_size;
 }
 
-int main(int argc, const char *argv[]){
+signed long dimTotaleFile(SizeFile * sizePerFile){
 
     DIR *dir;
     struct dirent *ent;
     signed long sizeTotFile=0;
-    SizeFile sizePerFile[10]={0," "};
-    
-    int p=4;
-    signed long bytePerProcesso[p];
     
     char cwd[PATH_MAX];
  
@@ -302,22 +160,100 @@ int main(int argc, const char *argv[]){
                 FILE *in_file;
                 char stringa[30]="file/";
                 strcat(stringa,ent->d_name);
-                //printf("%s\n",stringa);
-
                 sizePerFile[i].size=stat_filesize(stringa);
                 strcpy(sizePerFile[i].nomefile,stringa);
                 sizeTotFile+=sizePerFile[i].size;
-                printf("grandezza per file %s = %ld \n", stringa , sizePerFile[i].size);
                 i++;
             }
             
         }
     }
-    printf("size tot file %ld\n",sizeTotFile);
+    return sizeTotFile;
 
-    ripartizioneElementi(bytePerProcesso,sizeTotFile,p);
+}
 
-    SplitPerProcesso2 s = BPP2(bytePerProcesso,sizePerFile,p);
+int main (int argc, char *argv[]){
 
+    int numtasks, rank, source=0, tag=1;
+    int y=5, z=10;
+    MPI_Status stat;
+
+    MPI_Init(&argc,&argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &numtasks);
+
+    int count=3;
+    MPI_Datatype filePerProcType,oldtypes[count];
+    int blockcounts[count];
+    MPI_Aint offsets[count],lb,extent;
+
+    offsets[0] = offsetof(SplitPerProcesso2, rank);
+    oldtypes[0] = MPI_INT;
+    blockcounts[0] = 1;
+    MPI_Type_get_extent(MPI_INT,&lb,&extent);
+
+    offsets[1] = offsetof(SplitPerProcesso2, start);
+    oldtypes[1] = MPI_LONG;
+    blockcounts[1] = 2;
+    MPI_Type_get_extent(MPI_LONG,&lb,&extent);
+
+    offsets[2] = offsetof(SplitPerProcesso2, nomefile);
+    oldtypes[2] = MPI_CHAR;
+    blockcounts[2] = 16;
+    MPI_Type_get_extent(MPI_CHAR,&lb,&extent);
+
+    MPI_Type_create_struct(count, blockcounts, offsets, oldtypes, &filePerProcType); //creo il tipo struttura
+    MPI_Type_commit(&filePerProcType); 
+
+    long bytePerProcesso[rank];
+    long sizeTotFile=0;
+    SizeFile sizePerFile[numfile];
+    SplitPerProcesso2 s[splitprocesso];
+
+    sizeTotFile=dimTotaleFile(sizePerFile);
+
+    if(rank==0){
+        
+        ripartizioneElementi(bytePerProcesso,sizeTotFile,numtasks);
+        BPP2(s,bytePerProcesso,sizePerFile,numtasks);
+        int k=0; //indice di dove mi trovo all'interno della struttura
+        int startper0=0;
+        while(s[k].rank==0){
+                k++;
+                startper0++;
+            }
+            
+        int q=startper0; //da dove devo partire
+        for (int i=1; i<numtasks; i++){
+            int j=0; //quanti elementi
+            while(s[k].rank==i){
+                k++;
+                j++;
+            }
+            MPI_Send(&s[q], j , filePerProcType, i, tag, MPI_COMM_WORLD);
+            q=k;
+        }    
+
+        for(int i=0; i<startper0; i++){
+            
+        }     
+        //calcolo occorrenze
+        //recive
+
+    }else{
+        MPI_Recv(s, splitprocesso , filePerProcType, source, tag, MPI_COMM_WORLD, &stat);
+        int count;
+        MPI_Get_count(&stat, filePerProcType, &count);
+
+        for(int i=0; i<count; i++){
+            
+        }
+        //calcolo occorrenze
+        //send
+
+    }
+
+    MPI_Type_free(&filePerProcType);
+    MPI_Finalize();
     return 0;
 }
